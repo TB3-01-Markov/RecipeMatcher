5cd71b2e69041f5ec8f2e393f9a15e648654e51b
TB3-01-Markov <markovconstantin1@gmail.com>
2026-08-27 15:48:29 +0200

    11(Ingredients) in procces

---
+++ RecipeMatcher.Web/Controllers/IngredientsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeMatcher.Web.Models;
+using RecipeMatcher.Web.Data;
+namespace RecipeMatcher.Web.Controllers;
+
+public class IngredientsController(AppDbContext dbContext) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var ingredients = await dbContext.Ingredients.OrderBy(ingredient => ingredient.Name).ToListAsync();
+        return View(ingredients);
+    }
+    //Create GET
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    //Create POST
+    [HttpPost]
+    public async Task<IActionResult> Create(Ingredient ingredient)
+    {
+        if (!ModelState.IsValid) return View(ingredient);
+
+        dbContext.Ingredients.Add(ingredient);
+        await dbContext.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    //Edit GET
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var existingIngredient = await dbContext.Ingredients.FindAsync(id);
+        if (existingIngredient == null) return NotFound();
+
+        return View(existingIngredient);
+    }
+    //Edit POST
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Ingredient ingredient)
+    {
+        if (ingredient.Id != id) return NotFound();
+        if (!ModelState.IsValid) return View(ingredient);
+
+        var existingIngredient = await dbContext.Ingredients.FindAsync(id);
+        if (existingIngredient == null) return NotFound();
+
+        existingIngredient.Name = ingredient.Name;
+        await dbContext.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+    //Delete GET
+    [HttpGet]
+    [ActionName("Delete")]
+    public async Task<IActionResult> DeleteGet(int id)
+    {
+        var existingIngredient = await dbContext.Ingredients.FindAsync(id);
+        if (existingIngredient == null) return NotFound();
+
+        return View(existingIngredient);
+    }
+    //Delete POST
+    [HttpPost]
+    [ActionName("Delete")]
+    public async Task<IActionResult> DeletePostConfirmed(int id)
+    {
+        var existingIngredient = await dbContext.Ingredients.FindAsync(id);
+        if (existingIngredient == null) return NotFound();
+        else
+        {
+            dbContext.Ingredients.Remove(existingIngredient);
+            await dbContext.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
+    }
+    //Details GET
+    public async Task<IActionResult> Details(int id)
+    {
+        var ingredient = await dbContext.Ingredients.FindAsync(id);
+        if (ingredient is null) return NotFound();
+
+        return View(ingredient);
+    }
+}
+
+
+  
--- RecipeMatcher.Web/Data/AppDbContext.cs
+++ RecipeMatcher.Web/Data/AppDbContext.cs
@@ -3,8 +3,7 @@ using RecipeMatcher.Web.Models;
 
 namespace RecipeMatcher.Web.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options)
-    : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options): DbContext(options)
 {
     public DbSet<Recipe> Recipes => Set<Recipe>();
     public DbSet<Ingredient> Ingredients => Set<Ingredient>();
