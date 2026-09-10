7e0bfaf38fae48403dddb26d0065bb307e00cee5
TB3-01-Markov <markovconstantin1@gmail.com>
2026-09-10 16:29:13 +0200

    MatcherController and anderes++

--- RecipeMatcher.Web/ViewModels/EditRecipeViewModel.cs
+++ RecipeMatcher.Web/ViewModels/EditRecipeViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace RecipeMatcher.Web.ViewModels;
 
 public class EditRecipeViewModel
 {
     [Required]
+    [Range (1, int.MaxValue)] 
     public int Id { get; set; }
 
+    [Required]
     [StringLength(100)]
     public string Name { get; set; } = "";
 
+    [Required]
     [Range(1, 480)]
     public int PreparationMinutes { get; set; }
 
+    [Required]
     public IReadOnlyList<IngredientOptionViewModel> Ingredients { get; set; } = [];
 }
